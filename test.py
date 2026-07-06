9f095012b92580e55173a89262b01a1bc75c98d9
krushnakadam12 <krushnakadam9503@gmail.com>
2026-07-06 11:02:17 +0530

    prime number program pushing into dev

--- test.py
+++ test.py
@@ -1,13 +1,3 @@
 
-print("welcome to pune,pushing code into feature_dev")
-#prime number programe
-num = int(input("Enter the number: "))
-if num >1:
-    for i in range(2,num):
-        if num %i ==0:
-            print("Not a prime number")
-            break
-    else:
-        print("its prime number")
-else:
-    print("Enter the number grater then one")
+
+print("Krushns kadam")
